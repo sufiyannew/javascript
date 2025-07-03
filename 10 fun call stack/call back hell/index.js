a919aeb11204d5658dc81d18f1fcaae3ca72d8f6
sufiyannew <sufiyaaan7890@gmail.com>
2025-07-04 03:05:22 +0530

    update call back hell

--- 10 fun call stack/call back hell/index.js
+++ 10 fun call stack/call back hell/index.js
@@ -1,13 +1,24 @@
 let h1 = document.querySelector("h1");
 
-setTimeout(() => {
-    h1.style.color = "red"
-},1000);
+function changeColor(color, delay, nextColorChange) {
+    setTimeout(() => {
+        h1.style.color = color
+        nextColorChange();
+    },delay);
+}
 
-setTimeout(() => {
-    h1.style.color = "green"
-},2000);
+changeColor("red", 1000);
+changeColor("orange", 1000);
+changeColor("green", 1000);
 
-setTimeout(() => {
-    h1.style.color = "orange"
-},3000);
+// setTimeout(() => {
+//     h1.style.color = "red"
+// },1000);
+
+// setTimeout(() => {
+//     h1.style.color = "green"
+// },2000);
+
+// setTimeout(() => {
+//     h1.style.color = "orange"
+// },3000);
