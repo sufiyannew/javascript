bffec080f488cbd5cf4a6320f02cc234d76ce86b
sufiyannew <sufiyaaan7890@gmail.com>
2025-07-04 03:23:12 +0530

    add new folder promises

--- 10 fun call stack/call back hell/index.js
+++ 10 fun call stack/call back hell/index.js
@@ -4,12 +4,20 @@ function changeColor(color, delay, nextColorChange) {
     setTimeout(() => {
         h1.style.color = color
         nextColorChange();
+        if (nextColorChange) nextColorChange();
     },delay);
 }
 
-changeColor("red", 1000);
-changeColor("orange", 1000);
-changeColor("green", 1000);
+changeColor("red", 1000, () => {
+    changeColor("yellow", 1000, () => {
+        changeColor("blue", 1000, () => {
+            changeColor("green", 1000, () => {
+                changeColor("pink", 1000)
+            });
+        });
+    });
+});
+// call back nesting -> call back hell
 
 // setTimeout(() => {
 //     h1.style.color = "red"
