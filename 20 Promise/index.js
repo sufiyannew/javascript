1c2b3604f67817ceca7cddce7bdf95085fe8421b
sufiyannew <sufiyaaan7890@gmail.com>
2025-07-11 19:59:24 +0530

    add promises

--- 20 Promise/index.js
+++ 20 Promise/index.js
@@ -1,35 +1,48 @@
-function saveDb(data, success, failure) {
-  let internetSpeed = Math.floor(Math.random() * 10) + 1;
-  if (internetSpeed > 4) {
-    success();
-  } else {
-    failure();
-  }
+// function saveDb(data, success, failure) {
+//   let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//   if (internetSpeed > 4) {
+//     success();
+//   } else {
+//     failure();
+//   }
+// }
+// saveDb(
+//   "ansari salman",
+//   () => {
+//     console.log("success: your data was saved");
+//     saveDb(
+//       "ansari salman",
+//       () => {
+//         console.log("success2: your data2 was saved");
+//         saveDb(
+//           "ansari salman",
+//           () => {
+//             console.log("success3: your data3 was saved");
+//           },
+//           () => {
+//             console.log("failure3: weak connection data not saved");
+//           }
+//         );
+//       },
+//       () => {
+//         console.log("failure2: weak connection data not saved");
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("failure: weak connection data not saved");
+//   }
+// );
+
+function saveDb(data) {
+  return new promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("data was saved");
+    } else {
+      reject("weak connection");
+    }
+  });
 }
-saveDb(
-  "ansari salman",
-  () => {
-    console.log("success: your data was saved");
-    saveDb(
-      "ansari salman",
-      () => {
-        console.log("success2: your data2 was saved");
-        saveDb(
-          "ansari salman",
-          () => {
-            console.log("success3: your data3 was saved");
-          },
-          () => {
-            console.log("failure3: weak connection data not saved");
-          }
-        );
-      },
-      () => {
-        console.log("failure2: weak connection data not saved");
-      }
-    );
-  },
-  () => {
-    console.log("failure: weak connection data not saved");
-  }
-);
+
+saveDb("ansari salmna")
