79c7ad695e26733dc82c8d0fca84c95005e47b00
sufiyannew <sufiyaaan7890@gmail.com>
2025-07-11 19:43:03 +0530

    add call back xample

--- 20 Promise/index.js
+++ 20 Promise/index.js
@@ -1,1 +1,35 @@
-console.log("hi")
+function saveDb(data, success, failure) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 4) {
+    success();
+  } else {
+    failure();
+  }
+}
+saveDb(
+  "ansari salman",
+  () => {
+    console.log("success: your data was saved");
+    saveDb(
+      "ansari salman",
+      () => {
+        console.log("success2: your data2 was saved");
+        saveDb(
+          "ansari salman",
+          () => {
+            console.log("success3: your data3 was saved");
+          },
+          () => {
+            console.log("failure3: weak connection data not saved");
+          }
+        );
+      },
+      () => {
+        console.log("failure2: weak connection data not saved");
+      }
+    );
+  },
+  () => {
+    console.log("failure: weak connection data not saved");
+  }
+);
