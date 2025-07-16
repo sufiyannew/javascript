faadf94a3d3eadda8ddc6b9539334c102ad33e04
sufiyannew <sufiyaaan7890@gmail.com>
2025-07-16 21:28:50 +0530

    add result and error

--- 20 Promise/then catch/script.js
+++ 20 Promise/then catch/script.js
@@ -9,12 +9,31 @@ function saveDb(data) {
   });
 }
 
-let request = saveDb("ansari salmna");
-
-request
-  .then(() => {
-    console.log("promise resolved:");
+saveDb("ansari salmna") // this is method like call back hell mean nested call back 
+  .then((result) => {
+    console.log("promise 1 resolved:", result);
+    saveDb("ansari salmna")
+      .then(() => {
+        console.log("promise 2 resolved:");
+        saveDb("ansari salmna")
+          .then(() => {
+            console.log("promise 3 resolved:");
+            saveDb("ansari salmna")
+              .then(() => {
+                console.log("promise 4 resolved:");
+              })
+              .catch(() => {
+                console.log("promise rejected:");
+              });
+          })
+          .catch(() => {
+            console.log("promise rejected:");
+          });
+      })
+      .catch(() => {
+        console.log("promise rejected:");
+      });
   })
-  .catch(() => {
-    console.log("promise rejected:");
+  .catch((error) => {
+    console.log("promise rejected:", error);
   });
