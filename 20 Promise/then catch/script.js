e9f0edf86d4289e2767ab6b7a1981af7ae46aa10
sufiyannew <sufiyaaan7890@gmail.com>
2025-07-16 18:55:03 +0530

    add promise then catch method

---
+++ 20 Promise/then catch/script.js
@@ -0,0 +1,21 @@
+function saveDb(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("data was saved");
+    } else {
+      reject("weak connection");
+    }
+  });
+}
+
+let request = saveDb("ansari salmna");
+
+request
+  .then((message) => {
+    console.log("promise resolved:", message);
+  })
+  .catch((err) => {
+    console.log("promise rejected:", err);
+  });
+  
